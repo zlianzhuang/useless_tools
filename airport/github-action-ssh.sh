1ed572b9bb3c23bdddfa35447e32c9a1f1386dc3
lzzhang <zlianzhuang@163.com>
2025-10-16 18:14:10 +0800

    autocommit

--- airport/github-action-ssh.sh
+++ airport/github-action-ssh.sh
@@ -1,37 +1,15 @@
 #!/bin/bash
 
-if [[ -z "$TOKEN" ]]; then
-  echo "Please set 'TOKEN'"
-  exit 2
-fi
+set -x
 
+# the action user is "runner"
 if [[ -z "$USER_PASS" ]]; then
   echo "Please set 'USER_PASS' for user: $USER"
-  exit 3
+  exit 1
 fi
 
 echo "### Update user: $USER password ###"
 echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
 
-log=./airport/.log
-address=./airport/address
-
-rm -f $log
-./airport/ngrok authtoken "$TOKEN"
-./airport/ngrok tcp 22 --region jp --log "$log" &
-
-sleep 10
-HAS_ERRORS=$(grep "command failed" < $log)
-
-if [[ -z "$HAS_ERRORS" ]]; then
-  echo ""
-  echo "=========================================="
-  echo "To connect: $(grep -o -E "tcp://(.+)" < $log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
-  echo "=========================================="
-
-  date +'%Y-%m-%d %H:%M:%S' > $address
-  echo "To connect: $(grep -o -E "tcp://(.+)" < $log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")" >> $address
-else
-  echo "$HAS_ERRORS"
-  exit 4
-fi
+ssh -tt -fN -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=60 -R lzzhang-github-action:22:localhost:22 serveo.net
+sleep 1000000
