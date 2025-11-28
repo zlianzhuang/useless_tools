a267b49f8083edcc3aab217b0024f83a5d264ae2
lzzhang <zlianzhuang@163.com>
2025-11-28 13:03:46 +0800

    update server to dogyun

--- airport/github-action-ssh.sh
+++ airport/github-action-ssh.sh
@@ -10,6 +10,11 @@ fi
 
 echo "### Update user: $USER password ###"
 echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
+sudo apt update && apt install -y sshpass
 
-ssh -tt -fN -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=60 -R lzzhang-github-action:22:localhost:22 serveo.net
-sleep 1800
+#ssh -tt -fN -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=60 -R lzzhang-github-action:22:localhost:22 serveo.net
+sshpass -p "${SERVER_PASS}" ssh -tt -fN -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=60 -R 1234:localhost:22 root@206.237.0.43
+if [ "$?" = 0 ]
+then
+	sleep 1800
+fi
